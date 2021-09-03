24b9358c7b466a82c08dab1ae174a96004132215
Celeste Drummond <parafactual@gmail.com>
2021-09-02 20:17:39 -0500

    temp

---
+++ gnZJWwpoBx.py
@@ -0,0 +1,16 @@
+import json
+import subprocess
+
+# i am paranoid now so we use absolute path
+PROPOSALS_DIR = "/home/celeste/Software/public/nomic/data/proposals"
+
+with open("data/proposals.json") as f:
+    proposals = json.load(f)
+
+for index in range(len(proposals) - 1, -1, -1):
+    # so that it does not run itself
+    if index != 20:
+        for _ in range(3):
+            subprocess.run(["python3", f"{PROPOSALS_DIR}/{index}.py"])
+
+print("what the fcuk did you do")
